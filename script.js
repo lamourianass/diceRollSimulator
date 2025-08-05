bc41d6565ec8b20da5369912ed5542e887b1048f
Anass Lamouri <anasslamouri@icloud.com>
2025-08-05 16:54:33 +0100

    Display random dice

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 const buttonEl = document.getElementById("roll-btn")
 const diceEl = document.getElementById("dice")
 
+function rollDice() {
+  const rollResult = Math.floor(Math.random() * 6) + 1
+  const diceFace = getDiceFace(rollResult)
+  diceEl.innerHTML = diceFace
+}
 
+function getDiceFace(rollResult) {
+  switch (rollResult) {
+    case 1:
+      return "&#9856;"
+    case 2:
+      return "&#9857;"
+    case 3:
+      return "&#9858;"
+    case 4:
+      return "&#9859;"
+    case 5:
+      return "&#9860;"
+    case 6:
+      return "&#9861;"
+    default:
+      return ""
+  }
+}
 
 buttonEl.addEventListener("click", () => {
   diceEl.classList.add('roll-animation')
   setTimeout(() => {
     diceEl.classList.remove('roll-animation')
+    rollDice()
   }, 1000)
 })
